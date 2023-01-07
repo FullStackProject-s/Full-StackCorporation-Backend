de1f12a786e5b27dde7fb148daf021952706c347
Bol4onok <Bol4onok@mail.com>
2023-01-07 04:19:52 +0300

    Start refactoring create method in employee app

--- employee/views/service/response_developer_technologies.py
+++
@@ -1,9 +0,0 @@
-from general import PostResponse
-
-
-def developer_technologies_response(tech_list, message):
-    if tech_list:
-        return PostResponse.response_ok(
-            message
-        )
-    return PostResponse.not_found_response('Tech\'s not found')
