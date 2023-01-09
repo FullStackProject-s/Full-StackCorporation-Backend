37c5c2c2fc36745e66fdb90fc88743d7b2e37401
Bol4onok <Bol4onok@mail.com>
2023-01-09 15:29:33 +0300

    Fixed create_profiles method, now he return only news profiles, not all.

--- user/tests/utils/profile.py
+++ user/tests/utils/profile.py
@@ -3,12 +3,14 @@ from user.models.profile import Profile
 
 
 def create_profiles(profile_numbers: int, start=1) -> list[Profile]:
-    create_users_list(profile_numbers, start=start)
-
-    for index, profile in enumerate(
-            Profile.objects.all(),
+    users = create_users_list(profile_numbers, start=start)
+    profiles = []
+    for index, user in enumerate(
+            users,
             start=start
     ):
+        profile = Profile.objects.get(user=user)
         profile.about_user = f'profile something {index}'
         profile.save()
-    return Profile.objects.all()
+        profiles.append(profile)
+    return profiles
