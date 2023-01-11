029995d54da10d726043e9958d700449f007fd48
Bol4onok <Bol4onok@mail.com>
2023-01-11 13:57:09 +0300

    Reworked the user.Permission urls

--- src/user/urls/permission.py
+++ src/user/urls/permission.py
@@ -1,26 +1,25 @@
 from django.urls import path
-from user.views.permission import (
-    AllPermissionListAPIView,
-    PermissionRetrieveAPIView,
-    PermissionCreateAPIView,
-    PermissionDestroyAPIView,
-)
+from user.views import permission
 
 urlpatterns_permission = [
-    path('all-permissions/',
-         AllPermissionListAPIView.as_view(),
-         name='all-permissions'
-         ),
-    path('<int:pk>/',
-         PermissionRetrieveAPIView.as_view(),
-         name='permission'
-         ),
-    path('create-permission/',
-         PermissionCreateAPIView.as_view(),
-         name='create-permission'
-         ),
-    path('delete-permission/<int:pk>/',
-         PermissionDestroyAPIView.as_view(),
-         name='delete-permission'
-         ),
+    path(
+        'all-permissions/',
+        permission.AllPermissionListAPIView.as_view(),
+        name='all-permissions'
+    ),
+    path(
+        '<int:pk>/',
+        permission.PermissionRetrieveAPIView.as_view(),
+        name='permission'
+    ),
+    path(
+        'create-permission/',
+        permission.PermissionCreateAPIView.as_view(),
+        name='create-permission'
+    ),
+    path(
+        'delete-permission/<int:pk>/',
+        permission.PermissionDestroyAPIView.as_view(),
+        name='delete-permission'
+    ),
 ]
