70cce1959d2e96b74fdc06308515334688d380a8
Bol4onok <Bol4onok@mail.com>
2023-01-11 13:59:58 +0300

    Reworked the user.Profile urls

--- src/user/urls/profile.py
+++ src/user/urls/profile.py
@@ -1,31 +1,30 @@
 from django.urls import path
-from user.views.profile import (
-    AllProfileListAPIView,
-    ProfileRetrieveAPIView,
-    ProfileCreateAPIView,
-    ProfileDestroyAPIView,
-    ProfileUpdateAPIView
-)
+from user.views import profile
 
 urlpatterns_profile = [
-    path('all-profiles/',
-         AllProfileListAPIView.as_view(),
-         name='all-profiles',
-         ),
-    path('<int:pk>/',
-         ProfileRetrieveAPIView.as_view(),
-         name='profile',
-         ),
-    path('profile-create/',
-         ProfileCreateAPIView.as_view(),
-         name='create-profile',
-         ),
-    path('delete-profile/<int:pk>/',
-         ProfileDestroyAPIView.as_view(),
-         name='delete-profile',
-         ),
-    path('update-profile/<int:pk>',
-         ProfileUpdateAPIView.as_view(),
-         name='update-profile',
-         ),
+    path(
+        'all-profiles/',
+        profile.AllProfileListAPIView.as_view(),
+        name='all-profiles',
+    ),
+    path(
+        '<int:pk>/',
+        profile.ProfileRetrieveAPIView.as_view(),
+        name='profile',
+    ),
+    path(
+        'profile-create/',
+        profile.ProfileCreateAPIView.as_view(),
+        name='create-profile',
+    ),
+    path(
+        'delete-profile/<int:pk>/',
+        profile.ProfileDestroyAPIView.as_view(),
+        name='delete-profile',
+    ),
+    path(
+        'update-profile/<int:pk>',
+        profile.ProfileUpdateAPIView.as_view(),
+        name='update-profile',
+    ),
 ]
