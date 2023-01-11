2a2c95461590b2ace0ea2de5d828f974ddffd42b
Bol4onok <Bol4onok@mail.com>
2023-01-11 13:55:57 +0300

    Reworked the project.Team urls

--- src/project/urls/team.py
+++ src/project/urls/team.py
@@ -1,32 +1,31 @@
 from django.urls import path
-from project.views.team import (
-    AllTeamListAPIView,
-    TeamCreateAPIView,
-    TeamDestroyAPIView,
-    TeamUpdateAPIView,
-    TeamRetrieveAPIView
-)
+from project.views import team
 
 urlpatterns_team = [
-    path('all-team/',
-         AllTeamListAPIView.as_view(),
-         name='all-teams'
-         ),
-    path('<int:pk>',
-         TeamRetrieveAPIView.as_view(),
-         name='team'
-         ),
-    path('team-create/',
-         TeamCreateAPIView.as_view(),
-         name='create-team'
-         ),
-    path('delete-team/<int:pk>/',
-         TeamDestroyAPIView.as_view(),
-         name='delete-team'
-         ),
-    path('update-team/<int:pk>/',
-         TeamUpdateAPIView.as_view(),
-         name='update-team',
-         ),
+    path(
+        'all-team/',
+        team.AllTeamListAPIView.as_view(),
+        name='all-teams'
+    ),
+    path(
+        '<int:pk>',
+        team.TeamRetrieveAPIView.as_view(),
+        name='team'
+    ),
+    path(
+        'team-create/',
+        team.TeamCreateAPIView.as_view(),
+        name='create-team'
+    ),
+    path(
+        'delete-team/<int:pk>/',
+        team.TeamDestroyAPIView.as_view(),
+        name='delete-team'
+    ),
+    path(
+        'update-team/<int:pk>/',
+        team.TeamUpdateAPIView.as_view(),
+        name='update-team',
+    ),
 
 ]
