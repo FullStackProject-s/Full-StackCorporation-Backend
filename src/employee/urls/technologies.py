340c353a79899d2a13d975f3eb0a1da395827ada
Bol4onok <Bol4onok@mail.com>
2023-01-11 13:44:47 +0300

    Reworked the employee.Technologies urls

--- src/employee/urls/technologies.py
+++ src/employee/urls/technologies.py
@@ -1,32 +1,31 @@
 from django.urls import path
-from employee.views.technologies import (
-    AllTechnologiesListAPIView,
-    TechnologiesRetrieveAPIView,
-    TechnologiesDestroyAPIView,
-    TechnologiesCreateAPIView,
-    TechnologiesUpdateAPIView
-)
+from employee.views import technologies
 
 urlpatterns_techno = [
-    path('all-tech/',
-         AllTechnologiesListAPIView.as_view(),
-         name='all-tech'
-         ),
-    path('<int:pk>/',
-         TechnologiesRetrieveAPIView.as_view(),
-         name='technology'
-         ),
-    path('create-tech/',
-         TechnologiesCreateAPIView.as_view(),
-         name='create-tech'
-         ),
-    path('delete-tech/<int:pk>/',
-         TechnologiesDestroyAPIView.as_view(),
-         name='delete-tech'
-         ),
-    path('update-tech/<int:pk>/',
-         TechnologiesUpdateAPIView.as_view(),
-         name='update-tech'
-         ),
+    path(
+        'all-tech/',
+        technologies.AllTechnologiesListAPIView.as_view(),
+        name='all-tech'
+    ),
+    path(
+        '<int:pk>/',
+        technologies.TechnologiesRetrieveAPIView.as_view(),
+        name='technology'
+    ),
+    path(
+        'create-tech/',
+        technologies.TechnologiesCreateAPIView.as_view(),
+        name='create-tech'
+    ),
+    path(
+        'delete-tech/<int:pk>/',
+        technologies.TechnologiesDestroyAPIView.as_view(),
+        name='delete-tech'
+    ),
+    path(
+        'update-tech/<int:pk>/',
+        technologies.TechnologiesUpdateAPIView.as_view(),
+        name='update-tech'
+    ),
 
 ]
