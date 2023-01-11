98ab92cc485c1faa1bbd4aef8e98b305edb4badd
Bol4onok <Bol4onok@mail.com>
2023-01-11 13:45:42 +0300

    Reworked the employee.ProjectManager urls

--- src/employee/urls/project_manager.py
+++ src/employee/urls/project_manager.py
@@ -1,31 +1,30 @@
 from django.urls import path
-from employee.views.project_manager import (
-    AllProjectManagerListAPIView,
-    ProjectManagerRetrieveAPIView,
-    ProjectManagerDestroyAPIView,
-    ProjectManagerCreateAPIView,
-    ProjectManagerUpdateAPIView,
-)
+from employee.views import project_manager
 
 urlpatterns_manager = [
-    path('all-managers/',
-         AllProjectManagerListAPIView.as_view(),
-         name='all-managers'
-         ),
-    path('<int:pk>/',
-         ProjectManagerRetrieveAPIView.as_view(),
-         name='project-manager'
-         ),
-    path('create-manager/',
-         ProjectManagerCreateAPIView.as_view(),
-         name='create-manager'
-         ),
-    path('delete-manager/<int:pk>/',
-         ProjectManagerDestroyAPIView.as_view(),
-         name='delete-manager'
-         ),
-    path('update-manager/<int:pk>/',
-         ProjectManagerUpdateAPIView.as_view(),
-         name='update-manager'
-         ),
+    path(
+        'all-managers/',
+        project_manager.AllProjectManagerListAPIView.as_view(),
+        name='all-managers'
+    ),
+    path(
+        '<int:pk>/',
+        project_manager.ProjectManagerRetrieveAPIView.as_view(),
+        name='project-manager'
+    ),
+    path(
+        'create-manager/',
+        project_manager.ProjectManagerCreateAPIView.as_view(),
+        name='create-manager'
+    ),
+    path(
+        'delete-manager/<int:pk>/',
+        project_manager.ProjectManagerDestroyAPIView.as_view(),
+        name='delete-manager'
+    ),
+    path(
+        'update-manager/<int:pk>/',
+        project_manager.ProjectManagerUpdateAPIView.as_view(),
+        name='update-manager'
+    ),
 ]
