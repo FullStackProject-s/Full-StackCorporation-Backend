6e3c7e41f15ea07f31d72ae416cdb2d826c8482f
Bol4onok <Bol4onok@mail.com>
2023-01-11 14:00:40 +0300

    Reworked the user.CustomUser urls

--- src/user/urls/user.py
+++ src/user/urls/user.py
@@ -1,32 +1,30 @@
 from django.urls import path
-from user.views.user import (
-    UserCreateAPIView,
-    AllUsersListAPIView,
-    UserRetrieveAPIView,
-    UserDestroyAPIView,
-    UserUpdateAPIView,
-)
-
+from user.views import user
 
 urlpatterns_user = [
-    path('all-users/',
-         AllUsersListAPIView.as_view(),
-         name='all-users'
-         ),
-    path('<int:pk>/',
-         UserRetrieveAPIView.as_view(),
-         name='user'
-         ),
-    path('delete-user/<int:pk>/',
-         UserDestroyAPIView.as_view(),
-         name='delete-user'
-         ),
-    path('create-user/',
-         UserCreateAPIView.as_view(),
-         name='create-user'
-         ),
-    path('update-user/<int:pk>/',
-         UserUpdateAPIView.as_view(),
-         name='update-user',
-         ),
+    path(
+        'all-users/',
+        user.AllUsersListAPIView.as_view(),
+        name='all-users'
+    ),
+    path(
+        '<int:pk>/',
+        user.UserRetrieveAPIView.as_view(),
+        name='user'
+    ),
+    path(
+        'delete-user/<int:pk>/',
+        user.UserDestroyAPIView.as_view(),
+        name='delete-user'
+    ),
+    path(
+        'create-user/',
+        user.UserCreateAPIView.as_view(),
+        name='create-user'
+    ),
+    path(
+        'update-user/<int:pk>/',
+        user.UserUpdateAPIView.as_view(),
+        name='update-user',
+    ),
 ]
