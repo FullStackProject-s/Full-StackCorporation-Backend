f44a7ab40ce2a2fe00228a09139c86689885fc14
Bol4onok <Bol4onok@mail.com>
2023-01-13 02:03:29 +0300

    Refactored test for administrator

--- src/employee/signals/permissions_employee.py
+++ src/employee/signals/permissions_employee.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -7,12 +6,11 @@ from employee.models import (
     ProjectManager,
     Developer
 )
+from employee.signals.services import set_perms_for_employee
 from general.signals import suspending_receiver
 
 from user.models.consts import StaffRole
 from user.models import Permissions
-
-User = get_user_model()
 
 
 @receiver(post_save, sender=Administrator)
@@ -26,11 +24,10 @@ def set_admin_permission(
         perms, created = Permissions.objects.get_or_create(
             role_name=StaffRole.ADMIN
         )
-        instance.profile.user.staff_role = perms
-        instance.save()
+        set_perms_for_employee(instance, perms)
 
 
-@suspending_receiver(post_save, sender=ProjectManager)
+@receiver(post_save, sender=ProjectManager)
 def set_project_manager_permission(
         sender,
         instance: ProjectManager,
@@ -41,8 +38,7 @@ def set_project_manager_permission(
         perms, created = Permissions.objects.get_or_create(
             role_name=StaffRole.PRODUCT_MANAGER
         )
-        instance.profile.user.staff_role = perms
-        instance.save()
+        set_perms_for_employee(instance, perms)
 
 
 @receiver(post_save, sender=Developer)
@@ -56,9 +52,4 @@ def set_developer_permission(
         perms, created = Permissions.objects.get_or_create(
             role_name=StaffRole.DEVELOPER
         )
-
-        user = User.objects.get(pk=instance.profile.user.pk)
-        instance.profile.user.staff_role = perms
-        user.staff_role = perms
-        instance.save()
-        user.save()
+        set_perms_for_employee(instance, perms)
