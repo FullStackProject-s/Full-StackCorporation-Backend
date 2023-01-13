d0ef869b867487111ca18a57324a927224596105
Bol4onok <Bol4onok@mail.com>
2023-01-13 14:45:36 +0300

    Deleted organization signal, now organization set for project after create object in serializer method

--- src/organization/serializers/services/organization_perform.py
+++ src/organization/serializers/services/organization_perform.py
@@ -1,4 +1,13 @@
-def update_projects(instance, validated_data):
+from project.models import Project
+
+
+def _create_projects(instance):
+    if projects := instance.projects.all():
+        for project in projects:
+            Project.objects.get(pk=project.pk).set_organization(instance)
+
+
+def _update_projects(instance, validated_data):
     if projects := validated_data.get('projects', None):
         for project in instance.projects.all():
             project.organization = None
