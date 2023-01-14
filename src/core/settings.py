e692cd1fb5a85503fce16ecd0461eea8c2fb2556
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-14 19:51:04 +0300

    Added additional swagger type for developer specialty

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,8 +1,9 @@
+import os
+
+from datetime import timedelta
 from pathlib import Path
 
-import rest_framework.permissions
 from dotenv import load_dotenv
-import os
 
 load_dotenv(os.path.join(Path(__file__).resolve().parent, '.dev.env'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -34,8 +35,6 @@ THIRD_PARTY_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'drf_spectacular',
-    'model_bakery',
-
 ]
 PROJECT_APPS = [
     'user',
@@ -153,12 +152,52 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
-        # "rest_framework.permissions.IsAuthenticated",
-        'rest_framework.permissions.AllowAny',
+        "rest_framework.permissions.IsAuthenticated",
+        # 'rest_framework.permissions.AllowAny',
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
 
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    # 'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+
+    # custom
+    'AUTH_COOKIE': 'access_token',
+    # Cookie name. Enables cookies if value is set.
+    'AUTH_COOKIE_DOMAIN': None,
+    # A string like "example.com", or None for standard domain cookie.
+    'AUTH_COOKIE_SECURE': False,
+    # Whether the auth cookies should be secure (https:// only).
+    'AUTH_COOKIE_HTTP_ONLY': True,
+    # Http only cookie flag.It's not fetch by javascript.
+    'AUTH_COOKIE_PATH': '/',  # The path of the auth cookie.
+    'AUTH_COOKIE_SAMESITE': 'Lax',
+    # Whether to set the flag restricting cookie leaks on cross-site requests. This can be 'Lax', 'Strict', or None to disable the flag.
+}
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Web corporation api',
     'DESCRIPTION': 'web corp api',
