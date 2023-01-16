cf4a7d085497310d30c500549a90bb45e3dd63aa
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-16 18:58:17 +0300

    Rename directory and now common user don't active by default

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv(os.path.join(Path(__file__).resolve().parent.parent, '.dev.env'))
 
-from .config import *  # noqa
+from .additional_settings import *  # noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
