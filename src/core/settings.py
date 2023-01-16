409fd6668115554d2b7aecf77ba739bdf908b364
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-16 15:48:32 +0300

    DEBUG value moved in the env file

--- src/core/settings.py
+++ src/core/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG')))
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split()
 
