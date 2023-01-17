eb5958c9ec8fdad68259028a35a9969ac84ffe9f
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-17 18:17:33 +0300

    Update signals to set random avatar

--- src/user/signals/create_profiles.py
+++ src/user/signals/create_profiles.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 
+from general.models.utils import set_image_on_imagefield
 from user.models import (
     CustomUser,
     Profile
@@ -11,4 +12,10 @@ from general.signals import suspending_receiver
 @suspending_receiver(post_save, sender=CustomUser)
 def create_profile_for_user(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        instance_ = Profile.objects.create(user=instance)
+
+        set_image_on_imagefield(
+            instance.username,
+            instance.email,
+            imagefield=instance_.profile_avatar,
+        )
