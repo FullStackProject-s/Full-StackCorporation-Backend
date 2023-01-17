d467e6475cc65474964d276797b056b8f4b2492f
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-17 15:46:57 +0300

    Delete useless schema

--- src/general/schema/exclude.py
+++
@@ -1,6 +0,0 @@
-def preprocessing_filter_spec(endpoints):
-    filtered = []
-    for (path, path_regex, method, callback) in endpoints:
-        if not path.startswith("/auth/"):
-            filtered.append((path, path_regex, method, callback))
-    return filtered
