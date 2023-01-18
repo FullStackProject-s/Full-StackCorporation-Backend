a5e2689757097e0954e7bb4e1b0e6131a68cee8f
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-19 02:31:50 +0300

    Fill-data fix

--- src/general/views.py
+++ src/general/views.py
@@ -1,23 +1,24 @@
+from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 
 from general.models.utils import set_image_on_imagefield
-from user.models import CustomUser
-from django.conf import settings
+from user.models import Profile
 from general.tests.model_factory import *
 
 """
 ONLY FOR DEVELOPMENT
 """
+User = get_user_model()
 
 
 class CreateSuperUserView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
-        if not CustomUser.objects.filter(username='bol4onok').exists():
-            CustomUser.objects.create_superuser(
+        if not User.objects.filter(username='bol4onok').exists():
+            User.objects.create_superuser(
                 username='bol4onok',
                 password='bol4onok',
                 email='email@mail.com',
@@ -31,7 +32,6 @@ class CreateFillDataView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
-        settings.SUSPEND_SIGNALS = True
 
         developers = make_developer(40)
         admins = make_administrator(10)
@@ -40,6 +40,18 @@ class CreateFillDataView(APIView):
         projects = make_project(50)
         organization_ = make_organization(10)
 
+        teams = make_team(10)
+
+        completed_tasks = make_completed_tasks(10)
+        reassignments = make_reassignment(10)
+        task = make_task(10)
+
+        for profile in Profile.objects.all():
+            set_image_on_imagefield(
+                profile.user.username,
+                profile.user.email,
+                imagefield=profile.profile_avatar,
+            )
         for index, org in enumerate(organization_):
             for proj in projects:
                 org.projects.add(proj)
@@ -47,13 +59,18 @@ class CreateFillDataView(APIView):
                 org.members.add(dev.profile.user)
             for proj in project_managers:
                 org.members.add(proj.profile.user)
+
             org.members.add(admins[index].profile.user)
             set_image_on_imagefield(
                 org.organization_name,
                 imagefield=org.organization_avatar,
             )
             org.save()
-        teams = make_team(10)
+
+        for project in projects:
+            for team in teams:
+                project.teams.add(team)
+            project.save()
 
         for index, team in enumerate(teams):
             team.team_lead = developers[index]
@@ -61,8 +78,6 @@ class CreateFillDataView(APIView):
                 team.developers.add(dev)
             for proj in project_managers:
                 team.project_manager = proj
-        completed_tasks = make_completed_tasks(10)
-        reassignments = make_reassignment(10)
-        task = make_task(10)
+            team.save()
 
         return Response()
