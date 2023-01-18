2c99459dd10dea656e9fe7623684f817a61a9049
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-18 23:30:30 +0300

    Added favicon support

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -145,6 +145,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,7 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import (
+    path,
+    include
+)
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -38,7 +43,12 @@ urlpatterns += [
         name="swagger-ui"
     ),
 ]
-
+urlpatterns += [
+    path(
+        'favicon.ico',
+        RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico'))
+    )
+]
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
