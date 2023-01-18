02a7094b5ec9aff16b54b5eb5e52fce39d142df0
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-19 02:08:04 +0300

    Split signal on created profile and set profile avatar

--- src/user/signals/create_profiles.py
+++ src/user/signals/create_profiles.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from general.models.utils import set_image_on_imagefield
 from user.models import (
@@ -12,10 +13,14 @@ from general.signals import suspending_receiver
 @suspending_receiver(post_save, sender=CustomUser)
 def create_profile_for_user(sender, instance, created, **kwargs):
     if created:
-        instance_ = Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance)
 
+
+@receiver(post_save, sender=Profile)
+def set_avatar_for_profile(sender, instance, created, **kwargs):
+    if created:
         set_image_on_imagefield(
-            instance.username,
-            instance.email,
-            imagefield=instance_.profile_avatar,
+            instance.user.username,
+            instance.user.email,
+            imagefield=instance.profile_avatar,
         )
