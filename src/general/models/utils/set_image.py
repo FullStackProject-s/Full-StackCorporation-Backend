7cae40c2f4efb930012e80dd5f365d985f3552b1
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-19 02:34:47 +0300

    Added constraint to don't generate image if test run, in set image function

--- src/general/models/utils/set_image.py
+++ src/general/models/utils/set_image.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db.models.fields.files import ImageFieldFile
 
@@ -8,6 +9,9 @@ def set_image_on_imagefield(
         *args,
         imagefield: ImageFieldFile,
 ):
+    if hasattr(settings, 'TESTS_LAUNCHED'):
+        if settings.TESTS_LAUNCHED:
+            return
     img = generate_avatar(*args)
 
     img_name = f'{"".join([str(abs(hash(ele))) for ele in args])}.png'
