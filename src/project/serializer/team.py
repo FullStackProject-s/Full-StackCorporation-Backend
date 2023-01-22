33fefceeda66b1844b703c78986127d7b8699e54
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-22 18:00:47 +0300

    When team created it's automatically added in project

--- src/project/serializer/team.py
+++ src/project/serializer/team.py
@@ -1,16 +1,27 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema_field
+
 from employee.serializers import (
     DeveloperShowSerializer,
     ProjectManagerShowSerializer
 )
 
-from project.serializer.services import update_personal, \
-    create_personal  # noqa
+from project.serializer.services import (
+    update_personal,
+    create_personal
+)
 from project.serializer.generics import BaseTeamSerializer
 
 
 class TeamShowSerializer(BaseTeamSerializer):
     team_lead = DeveloperShowSerializer()
     project_manager = ProjectManagerShowSerializer()
+
+    @extend_schema_field(OpenApiTypes.STR)
+    def get_project(self, obj):
+        if obj.project:
+            return obj.project.project_name
+        return None
 
 
 class TeamSerializer(BaseTeamSerializer):
@@ -20,6 +31,11 @@ class TeamSerializer(BaseTeamSerializer):
     def create(self, validated_data):
         instance = super().create(validated_data)
         create_personal(instance)
+
+        project = validated_data.get('project')
+        project.teams.add(instance)
+        project.save()
+
         return instance
 
     def update(self, instance, validated_data):
