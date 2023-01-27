b1ea10bd6bfbdec5ecd5821498d027e172f19e14
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-28 01:25:30 +0300

    Added kwargs args in model factory

--- src/general/tests/model_factory/base_factory.py
+++ src/general/tests/model_factory/base_factory.py
@@ -3,11 +3,16 @@ from typing import Callable
 from model_bakery import baker
 
 
-def create_model_factory(path_model: str) -> Callable[[int], list | object]:
+def create_model_factory(
+        path_model: str,
+        **kwargs
+) -> Callable[[int], list | object]:
     def make_object_func(number: int) -> list[object] | object:
+        nonlocal kwargs
         objects = baker.make(
             path_model,
             _quantity=number,
+            **kwargs
         )
         if number == 1:
             return objects[0]
